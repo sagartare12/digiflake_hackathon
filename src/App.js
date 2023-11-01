2f07d0cca265b03f4738c47106598c5f9850ebcb
sagartare <sagartare2014@gmail.com>
2023-11-02 00:40:54 +0530

    React router implimented for home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
-
+import {Router,Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import logo from './../assets/logo.png'
+import { Link } from 'react-router-dom'
+import {FaUserAlt} from 'react-icons/fa'
 const Header = () => {
   return (
     <header className="fixed shadow-md w-full h-16 px-2 md:px-4">
         {/* desktop */}
         <div className="flex items-center h-full" >
+            <Link to={"/"}>
             <div className="h-14">
                 <img src={logo} alt="" className="h-full" />
+            </div>
+            </Link>
+            <div className="">
+              <nav></nav>
+              <div className="">
+                <FaUserAlt />
+              </div>
             </div>
         </div>
 
