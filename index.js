ce7b93cd351fe5dce361f5b4607e243951f80614
sagartare <sagartare2014@gmail.com>
2023-11-05 01:38:55 +0530

    Simple get method implimented for testing

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const express = requie('express')
+const express = require('express')
+const cors = require("cors")
+const mongoose = require("mongoose");
+
+const app=express();
+app.use(cors());
+app.use(express.json({limit:"10mb"}));
+
+const PORT = process.env.PORT || 8080;
+app.get("/",(req,res)=>{
+    res.send("server is running")
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`)
+})
+
+app.post("/signup",(req,res)=>{
+    console.log(req.body)
+})
