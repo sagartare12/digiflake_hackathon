b7a8d99f0b0054ad1d7d1c56e31e03ff61f9aa44
sagartare <sagartare2014@gmail.com>
2023-11-08 01:16:20 +0530

    Database conected with app

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const cors = require("cors")
+const mongoose = require("mongoose");
+const dotenv=require('dotenv');
+dotenv.config({path:'./config.env'})
+const {MongoClient} = require('mongodb');
+const app=express();
+
+
+//middlewares
+app.use(cors());
+app.use(express.json({limit:"10mb"}));
+
+
+
+
+//database connection
+
+async function main(){
+    const uri = "mongodb+srv://sagartare2014:<password>@cluster0.lznaela.mongodb.net/?retryWrites=true&w=majority";
+    const DB_URL=process.env.MONGODB_URL.replace('<password>',process.env.MONGODB_PASSWORD)
+
+    const client = new MongoClient(DB_URL);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect().then(()=>console.log("Database connected"));
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+main().catch(console.error);
+
+//listing databases 
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+//PORt configuration
+const PORT = process.env.PORT || 8080;
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`)
+})
+
+
+
+app.get("/",(req,res)=>{
+    res.send("server is running")
+})
+app.post("/signup",(req,res)=>{
+    console.log(req.body);
+})
