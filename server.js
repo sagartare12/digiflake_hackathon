23858f643707beb4db8229b4ab6d28ebc070e3a8
sagartare <sagartare2014@gmail.com>
2023-11-25 00:50:14 +0530

    vercel config1

--- server.js
+++ server.js
@@ -8,13 +8,9 @@ const app=require('./app')
 
 
 //middlewares
-app.use(
-  cors({
-    origin: "https://mern-restaurant-app-frontend.vercel.app",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-);
+app.use( cors());
+
+
 // app.use(express.json({limit:"10mb"}));
 
 
