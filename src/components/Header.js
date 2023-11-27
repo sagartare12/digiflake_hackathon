244d2765f225454b4e12b7dc11007d961ab9051b
sagartare <sagartare2014@gmail.com>
2023-11-27 18:03:13 +0530

    Logo size updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import logo from './../assets/logo.png'
+import logo from './../assets/sagar_shop.jpg'
 import { Link,useNavigate } from 'react-router-dom'
 import {FaShoppingCart} from 'react-icons/fa'
 import {FaCircleUser} from 'react-icons/fa6'
@@ -78,7 +78,7 @@ const Header = () => {
       {/* desktop */}
       <div className="flex items-center h-full justify-between">
         <Link to={"/"}>
-          <div className="h-10 md:h-14">
+          <div className="h-12 md:h-14">
             <img src={logo} alt="" className="h-full" />
           </div>
         </Link>
