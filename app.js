f676ccc1819359ff86e9ecc3995e9b139934680e
sagartare <sagartare2014@gmail.com>
2023-11-29 00:17:38 +0530

    Cors setting configured

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors = require("cors")
 
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://mern-restaurant-app-frontend.vercel.app',
     credentials: true, 
 }));
 
