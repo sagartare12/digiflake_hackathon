d1105a52de39b9be704b402a194ba44821c6ca93
sagartare <sagartare2014@gmail.com>
2023-11-29 01:46:40 +0530

    j1

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors = require("cors")
 
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: true,
     credentials: true, 
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
