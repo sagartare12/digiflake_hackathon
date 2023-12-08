c45af7d03e386b865d27a93d35179ec3d05a29b9
sagartare <sagartare2014@gmail.com>
2023-12-08 10:48:15 +0530

    Home card design changed finale

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const HomeCard=({name,image,category,price,isLoading,id})=>{
     console.log(name);
     return (
-        <div className=" bg-slate-100 shadow-md  md:h-[140px]">
+        <div className=" bg-slate-100 shadow-md  md:h-[150px]">
             {name ?
                 <div>
                     <Link to={`/menu/${id}`} onClick={()=>window.scrollTo({top:"0",behavior:"smooth"})}>
                      {/* <div className=" w-[100px] md:w-40 h-[100px] md:min-h-[140px] md:max-h-[150px]"> */}
-                        <div className="h-[100px] p-1 md:h-[140px] w-[100px] md:w-[140px] ">
+                        <div className="h-[100px] p-1 md:h-[150px] w-[100px] md:w-[150px] ">
                         <img src={image} alt="" className="h-full w-full"/>
                      </div>
                      {/* <h3 className="font-semibold text-slate-800 text-center capitalize text-sm md:text-lg">{name}</h3>
